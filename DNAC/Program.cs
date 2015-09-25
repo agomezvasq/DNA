5a10c4f19bf1668e71bcd14583cbf72e97337002
Andres <agomezv3@eafit.edu.co>
2015-09-24 19:10:11 -0500

    DNAC
    For creation of algorithms

---
+++ DNAC/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNAC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+
+        static int [] baseN(int s, int e)
+        {
+            if (e < s)
+                return null;
+            int[] b = new int[e - s];
+
+        }
+    }
+}
